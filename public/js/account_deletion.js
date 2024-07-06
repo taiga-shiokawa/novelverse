00de259282d70c3edec0de4adbb8d95b581a75a0
imy715 <155804262+imy715@users.noreply.github.com>
2024-07-07 08:48:42 +0900

    account_deletion
    html css js

---
+++ public/js/account_deletion.js
@@ -0,0 +1,29 @@
+
+//要素を取得
+const modal = document.querySelector('.js-modal'),
+      open = document.querySelector('.js-modal-open'),
+      closeButton = document.querySelector('.js-modal-close-button');
+      close = document.querySelector('.js-modal-close');
+
+//「開くボタン」をクリックしてモーダルを開く
+function modalOpen() {
+  modal.classList.add('is-active');
+}
+open.addEventListener('click', modalOpen);
+
+//「閉じるボタン」をクリックしてモーダルを閉じる
+function modalClose() {
+  modal.classList.remove('is-active');
+}
+close.addEventListener('click', modalClose);
+closeButton.addEventListener('click', modalClose);
+
+
+
+//「モーダルの外側」をクリックしてモーダルを閉じる
+function modalOut(e) {
+  if (e.target == modal) {
+    modal.classList.remove('is-active');
+  }
+}
+addEventListener('click', modalOut);
