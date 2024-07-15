a81dabf7b9dc3bc253a4cba501681a02916fe7f7
taiga-shiokawa <taiga.hr12@gmail.com>
2024-07-15 14:05:34 +0900

    ボイラーテンプレート、ejsライブラリ追加

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const app = express();
-const connectDB = require("./db/connect");
-const PORT = 3000;
-const DB_URL = "mongodb://localhost:27017/novelversedb";
+const connectDB = require("./db/connect");  // MongoDB接続
+const path = require("path");               // viewsフォルダのパスを指定
+const ejsMate = require("ejs-mate");        // テンプレートの共通部分の管理ライブラリ
 
+const app = express();
+const PORT = 3000;
+const DB_URL = "mongodb://localhost:27017/novelversedb";  // MongoDB名（ローカル）
+
+// MongoDB接続
 const start = async () => {
   try {
     await connectDB(DB_URL);
@@ -11,7 +15,12 @@ const start = async () => {
     console.log(err);
   }
 };
-
 start();
 
+// viewエンジン
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// サーバー接続
 app.listen(PORT, console.log("サーバーが起動しました"));
