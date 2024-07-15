9e98470c3c1e428e41c314eb75faf795db388d7b
taiga-shiokawa <taiga.hr12@gmail.com>
2024-07-15 15:18:11 +0900

    htmlからejsに引越し完了

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const connectDB = require("./db/connect");  // MongoDB接続
-const path = require("path");               // viewsフォルダのパスを指定
-const ejsMate = require("ejs-mate");        // テンプレートの共通部分の管理ライブラリ
+const connectDB = require("./db/connect");      // MongoDB接続
+const path = require("path");                   // viewsフォルダのパスを指定
+const ejsMate = require("ejs-mate");            // テンプレートの共通部分の管理ライブラリ
+const novelRouter = require("./routes/novels"); // 小説ルーター
+const userRouter = require("./routes/users");   // ユーザールーター
 
 const app = express();
 const PORT = 3000;
@@ -21,6 +23,18 @@ start();
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// フォームからのPOSTリクエストを受け取るためのミドルウェア
+app.use(express.urlencoded({ extended: true }));
+
+// publicフォルダを使用するためのミドルウェア
+app.use(express.static(path.join(__dirname, "public")));
+
+// 小説関連API
+app.use("/novel", novelRouter);
+
+// ユーザー関連API
+app.use("/user", userRouter);
 
 // サーバー接続
 app.listen(PORT, console.log("サーバーが起動しました"));
---
+++ routes/novels.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// ホーム画面へ遷移
+router.get("/home", (req, res) => {
+  res.render("novels/home");
+});
+
+// 「もっと見る」以降の小説一覧
+router.get("/list", (req, res) => {
+  res.render("novels/novel_list");
+});
+
+module.exports = router;
