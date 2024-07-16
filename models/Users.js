d604bd36d7201a22efa70a7b19f6988879233862
taiga-shiokawa <taiga.hr12@gmail.com>
2024-07-16 21:32:28 +0900

    Usersモデル作成

---
+++ models/Users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    requred: true,
+    unique: true,
+  },
+  self_introdaction: {
+    type: String,
+  },
+  birthday: {
+    type: Date,
+  },
+  image: [
+    {
+      url: String,
+      filename: String,
+    },
+  ],
+});
+
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User", userSchema);
