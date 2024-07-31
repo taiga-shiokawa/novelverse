2bbaddba66069ee511410d7ecc71d440a301e453
imy715 <155804262+imy715@users.noreply.github.com>
2024-07-31 23:01:07 +0900

    app.js戻す

--- app.js
+++ app.js
@@ -126,9 +126,7 @@ app.use(passport.session()); // ユーザー認証情報をセッションで維
 
 // すべてのルートでcsrfTokenを利用可能にする
 app.use((req, res, next) => {
-  console.log('あいうえお');
   req.session.csrfToken = req.csrfToken();
-  console.log('下記くけこ');
   next();
 });
 
