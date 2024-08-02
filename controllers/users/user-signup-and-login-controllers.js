9c2597e401ceb2d71d251697c2ea0f422aee5a78
taiga-shiokawa <taiga.hr12@gmail.com>
2024-08-02 13:32:08 +0900

    Resend 環境変数　再設定

--- controllers/users/user-signup-and-login-controllers.js
+++ controllers/users/user-signup-and-login-controllers.js
@@ -9,7 +9,13 @@ const userLoginValidate = require("../../utils/user-login-validation");
 const User = require("../../models/Users");
 
 // Resend環境変数
-const resend = new Resend(process.env.RESEND_API_KEY_HEROKU || process.env.RESEND_API_KEY);
+let resend;
+const apiKey = process.env.RESEND_API_KEY_HEROKU || process.env.RESEND_API_KEY;
+if (apiKey) {
+  resend = new Resend(apiKey);
+} else {
+  console.error('Resend API key is not set');
+}
 
 // アカウント作成画面へ遷移
 module.exports.goToAccountCreate = (req, res) => {
