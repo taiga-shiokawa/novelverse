43b82bdbb44e667a06c6f7f0e5e5fce3dfcef2d1
taiga-shiokawa <taiga.hr12@gmail.com>
2024-08-02 13:47:28 +0900

    Cloudinary Herokuに追加

--- cloudinary/cloudinary.js
+++ cloudinary/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME_HEROKU || process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY_HEROKU || process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_HEROKU || process.env.CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({
