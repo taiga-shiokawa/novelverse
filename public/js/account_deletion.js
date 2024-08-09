438e3c36f251995acf00e447ab82c272145db65b
imy715 <155804262+imy715@users.noreply.github.com>
2024-08-09 09:18:56 +0900

    管理者CSS

--- public/js/account_deletion.js
+++ public/js/account_deletion.js
@@ -1,28 +1,27 @@
-
-//要素を取得
+// 要素を取得
 const modal = document.querySelector('.js-modal'),
       open = document.querySelector('.js-modal-open'),
-      closeButton = document.querySelector('.js-modal-close-button');
+      closeButton = document.querySelector('.js-modal-close-button'),
       close = document.querySelector('.js-modal-close');
-//「開くボタン」をクリックしてモーダルを開く
+
+// 「開くボタン」をクリックしてモーダルを開く
 function modalOpen() {
   modal.classList.add('is-active');
 }
 open.addEventListener('click', modalOpen);
 
-//「閉じるボタン」をクリックしてモーダルを閉じる
-function modalClose() {
+// 「閉じるボタン」をクリックしてモーダルを閉じる
+function modalClose(e) {
+  e.preventDefault(); // aタグのデフォルト動作を防ぐ
   modal.classList.remove('is-active');
 }
 close.addEventListener('click', modalClose);
 closeButton.addEventListener('click', modalClose);
 
-
-
-//「モーダルの外側」をクリックしてモーダルを閉じる
+// 「モーダルの外側」をクリックしてモーダルを閉じる
 function modalOut(e) {
-  if (e.target == modal) {
+  if (e.target === modal) {
     modal.classList.remove('is-active');
   }
 }
-addEventListener('click', modalOut);
+document.addEventListener('click', modalOut);
