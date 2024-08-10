f7c24c054a71873c13063b2ef2435b517fc0d9c2
taiga-shiokawa <taiga.hr12@gmail.com>
2024-08-10 16:08:53 +0900

    app.js,  ユーザーアカウント設定関連　コードスタイリング

--- app.js
+++ app.js
@@ -4,39 +4,39 @@ if (process.env.NODE_ENV != "production") {
 }
 
 // 標準モジュール
-const path = require("path"); // viewsフォルダのファイルパスを指定するためのモジュール
+const path = require("path");  // viewsフォルダのファイルパスを指定するためのモジュール
 
 // サードパーティ(外部のライブラリなど)モジュール
-const connectDB = require("./db/connect"); // MongoDB接続のためにconnectファイルからインポート
-const express = require("express"); // Node.jsのWEBフレームワークであるExpressパッケージをインポート
-const ejsMate = require("ejs-mate"); // テンプレートの共通部分の管理ライブラリ
-const flash = require("connect-flash"); // req,res間で一時的に保持されたメッセージを表示してユーザーにフィードバックを行う
-const LocalStrategy = require("passport-local"); // Passport.jsのローカル認証をインポート
-const passport = require("passport"); // Passportライブラリのインポート
-const session = require("express-session"); // アプリのセッション管理機能を追加するためのミドルウェアの設定
-const methodOverride = require("method-override");
-const mongoSanitize = require("express-mongo-sanitize");
-const csrf = require("csurf");
-const cookieParser = require("cookie-parser");
-const { createClient } = require("redis");
-const RedisStore = require("connect-redis").default;
+const connectDB         = require("./db/connect");           // MongoDB接続のためにconnectファイルからインポート
+const csrf              = require("csurf");                  // CSRF対策
+const cookieParser      = require("cookie-parser");          // Cookie ヘッダーの解析
+const { createClient }  = require("redis");                  // Redis サーバーへの接続
+const express           = require("express");                // Node.jsのWEBフレームワークであるExpressパッケージをインポート
+const ejsMate           = require("ejs-mate");               // テンプレートの共通部分の管理ライブラリ
+const flash             = require("connect-flash");          // req,res間で一時的に保持されたメッセージを表示してユーザーにフィードバックを行う
+const LocalStrategy     = require("passport-local");         // Passport.jsのローカル認証をインポート
+const methodOverride    = require("method-override");        // put deleteメソッドを使用できるようにする
+const mongoSanitize     = require("express-mongo-sanitize"); // MongoDBインジェクション対策
+const passport          = require("passport");               // Passportライブラリのインポート
+const RedisStore        = require("connect-redis").default;  // ッションデータを Redis データベースに保存
+const session           = require("express-session");        // アプリのセッション管理機能を追加するためのミドルウェアの設定
 
 // ローカルモジュール
-const getGenreName = require("./common/genres"); // アプリ共通ヘッダーのナビゲーションにMongoDBから取得してきたジャンルを表示する
-const getAuthorName = require("./common/authors"); // アプリ共通ヘッダーのナビゲーションにMongoDBから取得してきた作家名を表示する
-const User = require("./models/Users"); // ユーザーの認証のためUserモデルをインポート
-const Admin = require("./models/Admins"); // ユーザーの認証のためUserモデルをインポート
-const userAccountRouter = require("./routes/users/user-account"); // ユーザーアカウント関連ルーター
-const userLogoutRouter = require("./routes/users/user-logout"); // ユーザーのログアウトルーター
-const novelRouter = require("./routes/novels/novels"); // 小説関連ルーター
-const novelManagementRouter = require("./routes/admins/admin-novel-management"); // 小説管理ルーター
-const userSignupAndLogin = require("./routes/users/user-signup-and-login"); // ユーザー登録とログインルーター
-const adminRouter = require("./routes/admins/admin"); // 管理者関連ルーター
-const adminUserManagementRouter = require("./routes/admins/admin-user-management"); // 管理者ユーザー管理関係ルーター
-const adminManagementRouter = require("./routes/admins/admin-management"); // 管理者の管理関係ルーター
-const adminAuthorRouter = require("./routes/admins/admin-author"); // 管理者の著者関係ルーター
-const ExpressError = require("./utils/ExpressError");
-const boardRouter = require("./routes/boards/boards");
+const Admin                       =   require("./models/Admins");                         // ユーザーの認証のためUserモデルをインポート
+const adminRouter                 =   require("./routes/admins/admin");                   // 管理者関連ルーター
+const adminUserManagementRouter   =   require("./routes/admins/admin-user-management");   // 管理者ユーザー管理関係ルーター
+const adminManagementRouter       =   require("./routes/admins/admin-management");        // 管理者の管理関係ルーター
+const adminAuthorRouter           =   require("./routes/admins/admin-author");            // 管理者の著者関係ルーター
+const boardRouter     =   require("./routes/boards/boards");
+const ExpressError    =   require("./utils/ExpressError");
+const getGenreName    =   require("./common/genres");         // アプリ共通ヘッダーのナビゲーションにMongoDBから取得してきたジャンルを表示する
+const getAuthorName   =   require("./common/authors");        // アプリ共通ヘッダーのナビゲーションにMongoDBから取得してきた作家名を表示する
+const novelManagementRouter   =   require("./routes/admins/admin-novel-management");  // 小説管理ルーター
+const novelRouter             =   require("./routes/novels/novels");                  // 小説関連ルーター
+const User                =   require("./models/Users");                              // ユーザーの認証のためUserモデルをインポート
+const userAccountRouter   =   require("./routes/users/user-account");                 // ユーザーアカウント関連ルーター
+const userLogoutRouter    =   require("./routes/users/user-logout");                  // ユーザーのログアウトルーター
+const userSignupAndLogin  =   require("./routes/users/user-signup-and-login");        // ユーザー登録とログインルーター
 
 const app = express();
 const PORT = 3000;
@@ -88,9 +88,9 @@ async function loadAuthors() {
 }
 
 // viewエンジン
-app.engine("ejs", ejsMate); // ejs拡張子を指定.
-app.set("view engine", "ejs"); // このアプリでejsをViewエンジンとして使用することを宣言.
-app.set("views", path.join(__dirname, "views")); // Viewsフォルダ配下のejsファイルの絶対パスの生成.
+app.engine("ejs", ejsMate);                       // ejs拡張子を指定.
+app.set("view engine", "ejs");                    // このアプリでejsをViewエンジンとして使用することを宣言.
+app.set("views", path.join(__dirname, "views"));  // Viewsフォルダ配下のejsファイルの絶対パスの生成.
 
 // JSONボディを解析するミドルウェア
 app.use(express.json());
@@ -100,9 +100,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // HTMLのフォーム送信によるbody部分に含まれるデータを解析しreq.bodyにパースするためのミドルウェア
 app.use(express.urlencoded({ extended: true }));
-
-// マルチパートフォームデータを解析するミドルウェア
-// const upload = multer({ storage: storage });
 
 // PUT、DELETEメソッドのため
 app.use(methodOverride("_method"));
@@ -124,8 +121,8 @@ const sessionConfig = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // Cookieの有効期限を1週間後に設定
-    maxAge: 1000 * 60 * 60 * 24 * 7, // Coolieの"最大"有効期限を1週間に設定
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,  // Cookieの有効期限を1週間後に設定
+    maxAge: 1000 * 60 * 60 * 24 * 7,                // Coolieの"最大"有効期限を1週間に設定
   },
 };
 
@@ -139,8 +136,8 @@ app.use(cookieParser());
 app.use(session(sessionConfig));
 app.use(flash());
 app.use(csrf({ cookie: true }));
-app.use(passport.initialize()); // パスポートを初期化し, ユーザー認証機能を有効にする.
-app.use(passport.session()); // ユーザー認証情報をセッションで維持する
+app.use(passport.initialize());   // パスポートを初期化し, ユーザー認証機能を有効にする.
+app.use(passport.session());      // ユーザー認証情報をセッションで維持する
 
 // CSRFトークンを生成し、レスポンスローカルに保存するミドルウェア
 app.use((req, res, next) => {
@@ -184,7 +181,7 @@ app.use((req, res, next) => {
 
 // ユーザーオブジェクトをシリアライズ（簡潔な形式に変換）してセッションに保存する
 passport.serializeUser((user, done) => {
-  done(null, { id: user._id, role: user.role }); // idとroleをセッションに保存
+  done(null, { id: user._id, role: user.role });  // idとroleをセッションに保存
 });
 
 /**
@@ -213,13 +210,13 @@ passport.deserializeUser(async (obj, done) => {
 // ジャンルとユーザー情報(セッション)を全てのルートで利用可能にするミドルウェア
 app.use(async (req, res, next) => {
   console.log(req.query);
-  res.locals.currentUser = req.user;
-  res.locals.success = req.flash("success");
-  res.locals.logoutSuccess = req.flash("logout-success");
-  res.locals.error = req.flash("error");
-  res.locals.loginError = req.flash("login-error");
-  res.locals.creationError = req.flash("creation-error");
-  res.locals.messages = req.flash();
+  res.locals.currentUser    = req.user;
+  res.locals.success        = req.flash("success");
+  res.locals.logoutSuccess  = req.flash("logout-success");
+  res.locals.error          = req.flash("error");
+  res.locals.loginError     = req.flash("login-error");
+  res.locals.creationError  = req.flash("creation-error");
+  res.locals.messages       = req.flash();
   if (!res.locals.genres) {
     res.locals.genres = await loadGenres();
   }
@@ -250,9 +247,9 @@ app.get("/app-info", async (req, res, next) => {
 
   try {
     if (res.locals.currentUser) {
-      const { id } = res.locals.currentUser; //ログイン中のユーザーのID
-      const loginUser = await User.findById(id); //ログイン中のユーザーの情報を全て取得
-      topImg = loginUser.image;
+      const { id }    = res.locals.currentUser;   //ログイン中のユーザーのID
+      const loginUser = await User.findById(id);  //ログイン中のユーザーの情報を全て取得
+      topImg          = loginUser.image;
     }
     res.render("users/app-info", { topImg });
   } catch (error) {
@@ -265,9 +262,9 @@ app.get("/app-info", async (req, res, next) => {
 app.get("/admin-info", async (req, res) => {
   let topImg = "";
   if (res.locals.currentUser) {
-    const { id } = res.locals.currentUser; //ログイン中のユーザーのID
-    const loginUser = await User.findById(id); //ログイン中のユーザーの情報を全て取得
-    topImg = loginUser.image;
+    const { id }    = res.locals.currentUser;      //ログイン中のユーザーのID
+    const loginUser = await User.findById(id);  //ログイン中のユーザーの情報を全て取得
+    topImg          = loginUser.image;
   }
   res.render("users/admin-info", { topImg });
 });
@@ -276,9 +273,9 @@ app.get("/admin-info", async (req, res) => {
 app.get("/terms-and-conditions", async (req, res) => {
   let topImg = "";
   if (res.locals.currentUser) {
-    const { id } = res.locals.currentUser; //ログイン中のユーザーのID
-    const loginUser = await User.findById(id); //ログイン中のユーザーの情報を全て取得
-    topImg = loginUser.image;
+    const { id }    = res.locals.currentUser;      //ログイン中のユーザーのID
+    const loginUser = await User.findById(id);  //ログイン中のユーザーの情報を全て取得
+    topImg          = loginUser.image;
   }
   res.render("common/terms_and_conditions", { topImg });
 });
@@ -287,24 +284,24 @@ app.get("/terms-and-conditions", async (req, res) => {
 app.get("/privacy-policy", async (req, res) => {
   let topImg = "";
   if (res.locals.currentUser) {
-    const { id } = res.locals.currentUser; //ログイン中のユーザーのID
-    const loginUser = await User.findById(id); //ログイン中のユーザーの情報を全て取得
-    topImg = loginUser.image;
+    const { id }    = res.locals.currentUser;      //ログイン中のユーザーのID
+    const loginUser = await User.findById(id);  //ログイン中のユーザーの情報を全て取得
+    topImg          = loginUser.image;
   }
   res.render("common/privacy_policy", { topImg });
 });
 
-app.use("/novel", novelRouter); // 小説関連API
-app.use("/novel/management", novelManagementRouter); // 小説管理API
-app.use("/user/account", userAccountRouter); // ユーザーアカウント関連API
-app.use("/user", userSignupAndLogin); // ユーザー登録とログインAPI
-app.use("/user", userLogoutRouter); // ユーザーログアウトAPI
-app.use("/admin", adminRouter); // 管理者自身関係API
+app.use("/admin", adminRouter);                               // 管理者自身関係API
+app.use("/admin-author", adminAuthorRouter);                  // 管理者著者関連API
+app.use("/admin-management", adminManagementRouter);          // 管理者の管理関係API
+app.use("/admin-novel-management", novelManagementRouter);    // 管理者小説関連API
 app.use("/admin-user-management", adminUserManagementRouter); // 管理者ユーザー管理関係API
-app.use("/admin-management", adminManagementRouter); // 管理者の管理関係API
-app.use("/admin-author", adminAuthorRouter); // 管理者著者関連API
-app.use("/admin-novel-management", novelManagementRouter); // 管理者小説関連API
 app.use("/board", boardRouter); // 掲示板
+app.use("/novel", novelRouter); // 小説関連API
+app.use("/user", userLogoutRouter);   // ユーザーログアウトAPI
+app.use("/user", userSignupAndLogin); // ユーザー登録とログインAPI
+app.use("/user/account", userAccountRouter);          // ユーザーアカウント関連API
+app.use("/novel/management", novelManagementRouter);  // 小説管理API
 
 // 共通エラーページ
 app.get("/error", (req, res) => {
@@ -316,11 +313,11 @@ app.use((err, req, res, next) => {
 
   // CSRFトークンエラーの処理
   console.error("CSRF error:", err);
+  console.error("Headers:", req.headers);
+  console.error("Session:", req.session);
   console.error("Request body:", req.body);
   console.error("CSRF token from form:", req.body._csrf);
   console.error("CSRF token from request:", req.csrfToken());
-  console.error("Headers:", req.headers);
-  console.error("Session:", req.session);
 
   req.flash(
     "error",
