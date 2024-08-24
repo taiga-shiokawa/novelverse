e00b9a765c5c75a3feabfce673818659da0c9a6b
imy715 <155804262+imy715@users.noreply.github.com>
2024-08-24 11:40:33 +0900

    会社概要レスポンシブ

--- routes/admins/admin.js
+++ routes/admins/admin.js
@@ -12,9 +12,9 @@ router.route('/admin-login')
     .get(Admin.renderAdminLogin )   //管理者ログイン画面に遷移
     .post(Admin.adminLogin )         //管理者ログイン
 
-router.route('/registration-admin')
-    .get( Admin.renderRegistrationAdmin )   //管理者新規登録画面に遷移
-    .post( Admin.registrationAdmin )        //管理者新規登録
+// router.route('/registration-admin')
+//     .get( Admin.renderRegistrationAdmin )   //管理者新規登録画面に遷移
+//     .post( Admin.registrationAdmin )        //管理者新規登録
 
 router.route('/dashboard')
     .get( adminIsLoggedIn , Admin.renderDashboardAdmin )   //管理者新規登録画面に遷移
